ccbe5ac8258534a5a6bdef8224056c6c45c028f0
Tewodros <tewodros212@gmail.com>
2022-02-21 15:39:13 -0600

    exe1 update

--- nodeAssignment/lab1/exercise1.js
+++ nodeAssignment/lab1/exercise1.js
@@ -1,31 +1,28 @@
 // Exercise 1
 // Write the necessary Node script to make this code work for all arrays: [1,2,3,4,5,6,7,8].//even(); // [2,4,6,8] [1,2,3,4,5,6,7,8].odd(); // [1,3,5,7] Test your code in Node.JS CL
 
-function even(arrays) {
+Array.prototype.even = function () {
     let even = [];
-    for (let i = 0; i < arrays.length; i++) {
-        if (arrays[i] % 2 == 0) {
-            even.push(arrays[i])
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] % 2 == 0) {
+            even.push(this[i])
         }
     }
     return even;
 }
-let arrays = [1, 2, 3, 4, 5, 6, 7, 8];
-let ev = even(arrays)
-console.log("even", ev)
 
-function odd(arrays) {
+console.log("even", [1, 2, 3, 4, 5, 6, 7, 8].even())
+
+Array.prototype.odd = function () {
     let odd = [];
-    for (let i = 0; i < arrays.length; i++) {
-        if (arrays[i] % 2 != 0) {
-            odd.push(arrays[i])
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] % 2 != 0) {
+            odd.push(this[i])
         }
     }
     return odd;
 }
-let array = [1, 2, 3, 4, 5, 6, 7, 8];
-let od = odd(arrays)
-console.log("odd ", od)
+console.log("odd ", [1, 2, 3, 4, 5, 6, 7, 8].odd())
 
 
 
